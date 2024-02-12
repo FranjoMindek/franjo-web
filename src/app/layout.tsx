52212023e79b6c8b9c4e5b39f20176a42eba1eff
Franjo Mindek <franjo.mindek@gmail.com>
2024-02-12 22:12:09 +0100

    insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import {Poppins} from 'next/font/google'
 import '../styles/globals.scss'
 import {ReactNode} from "react";
 import Navigation from "@/components/Navigation/Navigation";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const poppins = Poppins({
   weight: ['400', '500', '600', '700'],
@@ -17,15 +18,10 @@ export default async function RootLayout({
 }) {
   return (
     <html lang="en">
-    {/*<head>*/}
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
-      {/*<Suspense>*/}
-      {/*  <Analytics/>*/}
-      {/*</Suspense>*/}
-    {/*</head>*/}
       <body className={`${poppins.className} w-full min-h-screen flex flex-row items-stretch text-coffee`}>
         <Navigation/>
         {children}
+        <SpeedInsights/>
       </body>
     </html>
   )
