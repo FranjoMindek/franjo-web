e347ef3040c8811deaa30413531603711db8fb83
Franjo Mindek <franjo.mindek@gmail.com>
2024-02-16 23:19:30 +0100

    i hate google verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import '../styles/globals.scss'
 import {ReactNode} from "react";
 import Navigation from "@/components/Navigation";
 import { GoogleTagManager  } from '@next/third-parties/google'
+import Head from "next/head";
 
 const poppins = Poppins({
   weight: ['400', '500', '600', '700'],
@@ -18,6 +19,9 @@ export default async function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="google-site-verification" content="ntejZMO3jhEGTh8CNbdQ8piqlXt2n53vxjo6ATyCiyk"/>
+      </Head>
       <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID!}/>
       <body className={`${poppins.className} w-full min-h-[100dvh] flex flex-row items-stretch text-coffee`}>
         <Navigation/>
