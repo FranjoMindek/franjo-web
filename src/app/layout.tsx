08e66846939b1c3bb7f23a14d53796d414307abe
Franjo Mindek <franjo.mindek@gmail.com>
2024-02-16 22:24:17 +0100

    add gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import {Poppins} from 'next/font/google'
 import '../styles/globals.scss'
 import {ReactNode} from "react";
 import Navigation from "@/components/Navigation";
-import { GoogleAnalytics } from '@next/third-parties/google'
+import { GoogleTagManager  } from '@next/third-parties/google'
 
 const poppins = Poppins({
   weight: ['400', '500', '600', '700'],
@@ -18,8 +18,8 @@ export default async function RootLayout({
 }) {
   return (
     <html lang="en">
+      <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM!}/>
       <body className={`${poppins.className} w-full min-h-[100dvh] flex flex-row items-stretch text-coffee`}>
-        <GoogleAnalytics gaId="G-8N145L4M1T"/>
         <Navigation/>
         {children}
       </body>
