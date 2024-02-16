81f0dc6941d2edb1235d720bccb90e5bd24f0dc4
Franjo Mindek <franjo.mindek@gmail.com>
2024-02-16 23:22:53 +0100

    head head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import '../styles/globals.scss'
 import {ReactNode} from "react";
 import Navigation from "@/components/Navigation";
 import { GoogleTagManager  } from '@next/third-parties/google'
-import Head from "next/head";
 
 const poppins = Poppins({
   weight: ['400', '500', '600', '700'],
@@ -19,9 +18,9 @@ export default async function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
+      <head>
         <meta name="google-site-verification" content="ntejZMO3jhEGTh8CNbdQ8piqlXt2n53vxjo6ATyCiyk"/>
-      </Head>
+      </head>
       <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID!}/>
       <body className={`${poppins.className} w-full min-h-[100dvh] flex flex-row items-stretch text-coffee`}>
         <Navigation/>
